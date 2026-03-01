1cf8dc8e15b63c9a5a4988192f3094f55b971770
Shashwat_ghimire <67894307+Shashwat-SWAT@users.noreply.github.com>
2026-03-01 14:10:19 -0600

    Correcting the if statement & adding printf

--- Problem_One.c
+++ Problem_One.c
@@ -15,30 +15,33 @@ void score_catcher(int whole_score) {
     int the_sum_pts, Safety, FG, TD, TD_1pt_FG, TD_2pts;
 
     for (TD_2pts = 0; TD_2pts <= whole_score/8; TD_2pts++) {
+
         /* Loop runs for how many times you could add the 8 point in the score to match the
         total point. */
         for (TD_1pt_FG = 0; TD_1pt_FG <= whole_score/7; TD_1pt_FG++) {
+
         /* Loop runs for how many times you could add the 7 point in the score to match the
         total point. after the loop has run for TD_2pts. */
-            for (TD = 0; TD <= whole_score/6; TD++) {
+            for (TD = 0; TD <= whole_score/6; TD++) { // The loops below are added with same concept.
+
                 for (FG = 0; FG <= whole_score/3; FG++) {
+
                     for (Safety = 0; Safety <= whole_score/2; Safety++) {
-                        
+
+                        the_sum_pts = (Safety*2) + (FG*3) + (TD*6) + (TD_1pt_FG*7) + (TD_2pts*8);
+                        //Multiplying the points value each with their number.
+
+                        if (the_sum_pts == whole_score) {
+                            printf("");
+                        }
                     }
                 }
             }
-        
-
         }
-
     }
-    
 }
 
-            // if (the_sum_pts == whole_score) {
-            //     the_sum_pts = (Safety*2) + (FG*3) + (TD*6) + (TD_1pt_FG*7) + (TD_2pts*8);
-            //     //Multiplying the points value each with their number.
-            // }
+
 
 int main() {
     int score;
