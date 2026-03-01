dc08059dab073d8ca13c2e064f0b9d2d90ba944d
Shashwat_ghimire <67894307+Shashwat-SWAT@users.noreply.github.com>
2026-03-01 15:06:50 -0600

    Building in progresss
    I have broken down each part as function for reusability.

--- Problem_Two.c
+++ Problem_Two.c
@@ -1,20 +1,62 @@
 #include <stdio.h>
-
-int temp_convert() {
-    
-    
-}
+#include <ctype.h> // lets me use tolower(), tohigher()
 
 /*
-1. float celsius_to_fahrenheit (float celsius)
 Converts Celsius to Fahrenheit using the formula: F = (9/5)C + 32
-2. float fahrenheit_to_celsius (float fahrenheit)
 Converts Fahrenheit to Celsius using the formula: C = (5/9)(F - 32)
-3. float celsius_to_kelvin (float celsius)
 Converts Celsius to Kelvin using the formula: K = C + 273.15
-4. float kelvin_to_celsius (float kelvin)
 Converts Kelvin to Celsius using the formula: C = K - 273.15
 */
+
+int any_to_celcius(char chg_type_tmp, float tmp_val) {
+
+    float out_temp;
+
+    if (chg_type_tmp == 'f') {
+        //C = (5/9)(F - 32)
+    }
+
+    else if (chg_type_tmp == 'k') {
+        //C = K - 273.15
+    }
+
+    else {
+
+    }
+}
+
+int any_to_kelvin(char chg_type_tmp, float tmp_val) {
+
+    float out_temp;
+
+    if (chg_type_tmp == '')
+
+}
+
+int any_to_Fahrenheit(char chg_type_tmp, float tmp_val) {
+
+}
+
+int temp_convert(char in_t, char out_t, float in_temp) {
+
+    float out_temp;
+
+    if (in_t == out_t) {
+        out_temp = in_temp;
+    }
+
+    if (out_t == 'f') {
+
+    }
+
+    else if (out_t == 'c') {
+
+    }
+
+    else { //kelvin
+
+    }
+}
 
 int main() {
 
@@ -22,9 +64,9 @@ int main() {
     float temperature_in, temperature_out;
 
     printf("Enter the initials (F,C,K), Choose the temperature scale of the input value (Fahrenheit, Celsius, or Kelvin): ");
-    temp_in = getchar();
+    temp_in = tolower(getchar());
     printf("Enter the initials (F,C,K), Choose the temperature scale of the output value (Fahrenheit, Celsius, or Kelvin): ");
-    temp_out = getchar();
+    temp_out = tolower(getchar());
 
     printf("Enter the temperature: ");
     scanf("%f",temperature_in);
