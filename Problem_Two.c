79ec59c076c0e8b5fb63d69fac7d78e78b0c238a
Shashwat_ghimire <67894307+Shashwat-SWAT@users.noreply.github.com>
2026-03-01 19:20:40 -0600

    Fixes & Optimizing

--- Problem_Two.c
+++ Problem_Two.c
@@ -2,46 +2,23 @@
 #include <ctype.h> // lets me use tolower(), tohigher()
 
 /*
-Converts Celsius to Fahrenheit using the formula: F = (9/5)C + 32
-Converts Fahrenheit to Celsius using the formula: C = (5/9)(F - 32)
+Converts Celsius to Fahrenheit using the formula: F = (9.0/5.0)C + 32
+Converts Fahrenheit to Celsius using the formula: C = (5.0/9.0)(F - 32)
 Converts Celsius to Kelvin using the formula: K = C + 273.15
 Converts Kelvin to Celsius using the formula: C = K - 273.15
 */
 
-int any_to_Celcius(char initail_type_tmp, float tmp_val) {
+float any_to_Celsius(char initial_type_tmp, float tmp_val) {
 
     float out_temp;
 
-    if (initail_type_tmp == 'f') {
-        //C = (5/9)(F - 32)
-        out_temp = (5/9)*(tmp_val - 32);
+    if (initial_type_tmp == 'f') {
+        //C = (5.0/9.0)(F - 32)
+        out_temp = (5.0/9.0)*(tmp_val - 32);
     }
 
-    else if (initail_type_tmp == 'k') {
+    else if (initial_type_tmp == 'k') {
         //C = K - 273.15
-        out_temp = tmp_val - - 273.15;
-    }
-
-    else {
-        out_temp = tmp_val;
-    }
-
-    return out_temp;
-}
-
-int any_to_Kelvin(char initail_type_tmp, float tmp_val) {
-
-    float out_temp;
-
-    if (initail_type_tmp == 'c') {
-        //K = C + 273.15
-        out_temp = tmp_val + 273.15;
-    }
-
-    else if (initail_type_tmp == 'f') {
-        //C = (5/9)(F - 32)
-        //K = C + 273.15
-        tmp_val = (5/9)*(tmp_val - 32);
         out_temp = tmp_val - 273.15;
     }
 
@@ -52,20 +29,43 @@ int any_to_Kelvin(char initail_type_tmp, float tmp_val) {
     return out_temp;
 }
 
-int any_to_Fahrenheit(char initail_type_tmp, float tmp_val) {
+float any_to_Kelvin(char initial_type_tmp, float tmp_val) {
 
     float out_temp;
 
-    if (initail_type_tmp == 'c') {
-        //F = (9/5)C + 32
-        out_temp = (9/5)*tmp_val + 32;
+    if (initial_type_tmp == 'c') {
+        //K = C + 273.15
+        out_temp = tmp_val + 273.15;
     }
 
-    else if (initail_type_tmp == 'k') {
+    else if (initial_type_tmp == 'f') {
+        //C = (5.0/9.0)(F - 32)
+        //K = C + 273.15
+        tmp_val = (5.0/9.0)*(tmp_val - 32);
+        out_temp = tmp_val + 273.15;
+    }
+
+    else {
+        out_temp = tmp_val;
+    }
+
+    return out_temp;
+}
+
+float any_to_Fahrenheit(char initial_type_tmp, float tmp_val) {
+
+    float out_temp;
+
+    if (initial_type_tmp == 'c') {
+        //F = (9.0/5.0)C + 32
+        out_temp = (9.0/5.0)*tmp_val + 32;
+    }
+
+    else if (initial_type_tmp == 'k') {
         //C = K - 273.15
-        //F = (9/5)C + 32
+        //F = (9.0/5.0)C + 32
         tmp_val = tmp_val - 273.15;
-        out_temp = (9/5)*tmp_val + 32;
+        out_temp = (9.0/5.0)*tmp_val + 32;
     }
 
     else {
@@ -76,18 +76,18 @@ int any_to_Fahrenheit(char initail_type_tmp, float tmp_val) {
 
 }
 
-int temp_convert(char in_t, char out_t, float in_temp) {
+// End of conversion functions~
+
+float temp_convert(char in_t, char out_t, float in_temp) {
 
     float out_temp;
 
     if (out_t == 'f') {
         out_temp = any_to_Fahrenheit (in_t, in_temp);
-
     }
 
     else if (out_t == 'c') {
-        out_temp = any_to_Celcius (in_t, in_temp);
-
+        out_temp = any_to_Celsius (in_t, in_temp);
     }
 
     else { //kelvin
@@ -96,6 +96,7 @@ int temp_convert(char in_t, char out_t, float in_temp) {
 
     return out_temp;
 }
+// main functions that uses other minor functions to conversion ends here.
 
 void caution_print(char ini_tmp, float temp_val) {
     
@@ -107,21 +108,22 @@ void caution_print(char ini_tmp, float temp_val) {
     • Extreme Heat: Above 35°C (or equivalent)
     */
 
-    temp_val = any_to_Celcius(ini_tmp, temp_val);
+    temp_val = any_to_Celsius(ini_tmp, temp_val);
+    printf("\n");
     
     if (temp_val < 0) {
         printf ("It is freezing outside! Better not go anywhere!");
     }
 
-    else if ((temp_val >= 0) && (temp_val <= 10)) {
-        printf ("It's preety cold, wear a jacket before you head out.");
+    else if (temp_val <= 10) {
+        printf ("It's pretty cold, wear a jacket before you head out.");
     }
 
-    else if ((temp_val >= 10) && (temp_val <= 25)) {
-        printf ("Good waether today!");
+    else if (temp_val <= 25) {
+        printf ("Good weather today!");
     }
 
-    else if ((temp_val >= 25) && (temp_val <= 35)) {
+    else if (temp_val <= 35) {
         printf ("Dont forget the sunscreen!");
     }
 
@@ -130,48 +132,15 @@ void caution_print(char ini_tmp, float temp_val) {
     }
 }
 
-int same_type_enter(char*temp_in_type, char*temp_out_type) {
+void type_check(char*temp_in_type, char*temp_out_type);
 
-    char choice;
+void type_check(char*temp_in_type, char*temp_out_type) {
 
-    type_check(temp_in_type, temp_out_type);
-
-    if (*temp_in_type == *temp_out_type) {
-        printf("Warning! You enetered the same type for input and output!");
-        printf("Would you like to re-enter(Y/n): ");
-        choice = tolower(getchar());
-        scanf(" %c", &choice);
-        choice = tolower(choice);
-    }
-
-    if (choice == 'y') {
-        printf("Enter the initials (F,C,K), Choose the temperature scale of the input value (Fahrenheit, Celsius, or Kelvin): ");
-        scanf(" %c", temp_in_type);
-        *temp_in_type = tolower(*temp_in_type);
-        
-        printf("Enter the initials (F,C,K), Choose the temperature scale of the output value (Fahrenheit, Celsius, or Kelvin): ");
-        scanf(" %c", temp_out_type);
-        *temp_out_type = tolower(*temp_out_type);
-    }
-    
-    if ((*temp_in_type == *temp_out_type) && (choice == 'y')) {
-        same_type_enter(temp_in_type, temp_out_type);
-    }
-
-}
-
-int type_check(char*temp_in_type, char*temp_out_type) {
-
-    char choice;
-    int condition = (((*temp_in_type == 'c') || (*temp_in_type == 'k') || (*temp_in_type == 'f')) && ((*temp_out_type == 'c') || (*temp_out_type == 'k') || (*temp_out_type == 'f')));
+    int condition = !(((*temp_in_type == 'c') || (*temp_in_type == 'k') || (*temp_in_type == 'f')) && ((*temp_out_type == 'c') || (*temp_out_type == 'k') || (*temp_out_type == 'f')));
     
     // (((*temp_in_type == 'c') || (*temp_in_type == 'k') || (*temp_in_type == 'f')) && ((*temp_out_type == 'c') || (*temp_out_type == 'k') || (*temp_out_type == 'f')));
-    if (!(condition)) {
-        printf("Warning! Wrong input for either input and output type!");
-        choice = 'y';
-    }
-
-    if (choice == 'y') {
+    while (condition) {
+        printf("Warning! Wrong input for either input and output type!\n");
         printf("Enter the initials (F,C,K), Choose the temperature scale of the input value (Fahrenheit, Celsius, or Kelvin): ");
         scanf(" %c", temp_in_type);
         *temp_in_type = tolower(*temp_in_type);
@@ -179,14 +148,9 @@ int type_check(char*temp_in_type, char*temp_out_type) {
         printf("Enter the initials (F,C,K), Choose the temperature scale of the output value (Fahrenheit, Celsius, or Kelvin): ");
         scanf(" %c", temp_out_type);
         *temp_out_type = tolower(*temp_out_type);
+        condition = !(((*temp_in_type == 'c') || (*temp_in_type == 'k') || (*temp_in_type == 'f')) && ((*temp_out_type == 'c') || (*temp_out_type == 'k') || (*temp_out_type == 'f')));
     }
     
-    if (!(condition)) {
-        type_check(temp_in_type, temp_out_type);
-    }
-
-    same_type_enter(temp_in_type, temp_out_type);
-
 }
 
 int main() {
@@ -202,15 +166,14 @@ int main() {
     scanf(" %c", &temp_out);
     temp_out = tolower(temp_out);
 
-
-    same_type_enter(&temp_in, &temp_out);
+    type_check(&temp_in, &temp_out);
     
     printf("Enter the temperature: ");
-    scanf("%f",temperature_in);
+    scanf(" %f", &temperature_in);
 
     temperature_out = temp_convert(temp_in, temp_out, temperature_in);
-    printf("%2.2f %2c is %2.2f %2c", temperature_in, temp_in, temperature_out, temp_out);
 
+    printf("%2.2f %2c is %2.2f %2c", temperature_in, temp_in, temperature_out, temp_out);
     caution_print(temp_in, temperature_in);
 
     return 0;
